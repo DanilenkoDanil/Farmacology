1712df9d7bea9b9f9dd62512d338ea9c1ce32bbc
Данил <danilenkodanil74@gmail.com>
2025-01-19 17:49:51 +0200

    animation none

--- js/scripts.js
+++ js/scripts.js
@@ -18,14 +18,12 @@ function loadSlides(setName) {
     const slidesContainer = document.getElementById('slides-container');
     const reveal = document.querySelector('.reveal');
 
-    // Удаляем старые слайды
     Array.from(slidesContainer.children).forEach((slide, index) => {
         if (index > 0) {
             slidesContainer.removeChild(slide);
         }
     });
 
-    // Генерация новых слайдов
     const newSlides = generateSlides(mapping[setName]);
     const tempDiv = document.createElement('div');
     tempDiv.innerHTML = newSlides;
@@ -34,14 +32,11 @@ function loadSlides(setName) {
         slidesContainer.appendChild(slide);
     });
 
-    // Удаляем и пересоздаем Reveal.js
     reveal.style.display = 'block';
-    Reveal.destroy(); // Полностью уничтожаем текущую презентацию
-    Reveal.initialize({
-        width: '100%',
-        height: '100%',
-    });
 
-    Reveal.slide(1); // Перейти на первый слайд
+    Reveal.layout();
+    Reveal.sync();
+    Reveal.slide(1);
 }
+
 
