c6767000ffe77fa337b6f09583f1e19ee65968a3
Данил <danilenkodanil74@gmail.com>
2025-01-19 04:19:46 +0200

    mapping

--- js/mapping.js
+++ js/mapping.js
@@ -1,12 +1,509 @@
 const mapping = {
     pediatr: [
-        { image: "/slides/Asacol/A (0).png" },
-        { image: "/slides/Asacol/A (1).png" },
-        { image: "/slides/Asacol/A (2).png" },
+        {
+            image: "/slides/Enterol2560/E (1).png",
+            type: "Enterol",
+            special_animation: 'Base'
+        },
+        {
+            image: "/slides/Enterol2560/E (2).png",
+            type: "Enterol",
+            special_animation: 'Base'
+        },
+        {
+            image: "/slides/Enterol2560/E (25).png",
+            type: "Enterol",
+            special_animation: 'Base'
+        },
+        {
+            image: "/slides/Enterol2560/E (5).png",
+            type: "Enterol",
+            special_animation: 'Base'
+        },
+        {
+            image: "/slides/Enterol2560/E (20).png",
+            type: "Enterol",
+            special_animation: 'Base'
+        },
+        {
+            image: "/slides/Enterol2560/E (19).png",
+            type: "Enterol",
+            special_animation: 'Base'
+        },
+        {
+            image: "/slides/Enterol2560/E (21).png",
+            type: "Enterol",
+            special_animation: 'Base'
+        },
+        {
+            image: "/slides/Enterol2560/E (26).png",
+            type: "Enterol",
+            special_animation: 'Base'
+        },
+        {
+            image: "/slides/Marimer2560/M (8).png",
+            type: "Marimer",
+            special_animation: 'Base'
+        },
+        {
+            image: "/slides/Marimer2560/M (9).png",
+            type: "Marimer",
+            special_animation: 'Base'
+        },
+        {
+            image: "/slides/Marimer2560/M (11).png",
+            type: "Marimer",
+            special_animation: 'Base'
+        },
+        {
+            image: "/slides/Marimer2560/M (13).png",
+            type: "Marimer",
+            special_animation: 'Base'
+        },
+        {
+            image: "/slides/Marimer2560/M (14).png",
+            type: "Marimer",
+            special_animation: 'Base'
+        },
+        {
+            image: "/slides/Marimer2560/M (6).png",
+            type: "Marimer",
+            special_animation: 'Base'
+        },
+        {
+            image: "/slides/Marimer2560/M (5).png",
+            type: "Marimer",
+            special_animation: 'Base'
+        },
+        {
+            image: "/slides/Marimer2560/M (23).png",
+            type: "Marimer",
+            special_animation: 'Base'
+        },
+        {
+            image: "/slides/Marimer2560/M (2).png",
+            type: "Marimer",
+            special_animation: 'Base'
+        },
+        {
+            image: "/slides/Marimer2560/M (18).png",
+            type: "Marimer",
+            special_animation: 'Base'
+        },
+        {
+            image: "/slides/Marimer2560/M (16).png",
+            type: "Marimer",
+            special_animation: 'Base'
+        },
+        {
+            image: "/slides/Marimer2560/M (17).png",
+            type: "Marimer",
+            special_animation: 'Base'
+        },
+        {
+            image: "/slides/Marimer2560/M (23).png",
+            type: "Marimer",
+            special_animation: 'Base'
+        },
+        {
+            image: "/slides/Bebykol2560/Б (3).png",
+            type: "Bebykol",
+            special_animation: 'Base'
+        },
+        {
+            image: "/slides/Bebykol2560/Б (4).png",
+            type: "Bebykol",
+            special_animation: 'Base'
+        },
+        {
+            image: "/slides/Otipax2560/O (7).png",
+            type: "Otipax",
+            special_animation: 'Base'
+        },
+        {
+            image: "/slides/Otipax2560/O (2).png",
+            type: "Otipax",
+            special_animation: 'Base'
+        }
     ],
     gastro: [
-        { image: "/slides/Asacol/A (0).png" },
-        { image: "/slides/Asacol/A (1).png" },
-        { image: "/slides/Asacol/A (2).png" },
+        {
+            image: "/slides/Enterol2560/E (1).png",
+            type: "Enterol",
+            special_animation: 'Base'
+        },
+        {
+            image: "/slides/Enterol2560/E (2).png",
+            type: "Enterol",
+            special_animation: 'Base'
+        },
+        {
+            image: "/slides/Enterol2560/E (20).png",
+            type: "Enterol",
+            special_animation: 'Base'
+        },
+        {
+            image: "/slides/Enterol2560/E (19).png",
+            type: "Enterol",
+            special_animation: 'Base'
+        },
+        {
+            image: "/slides/Enterol2560/E (21).png",
+            type: "Enterol",
+            special_animation: 'Base'
+        },
+        {
+            image: "/slides/Enterol2560/E (22).png",
+            type: "Enterol",
+            special_animation: 'Base'
+        },
+        {
+            image: "/slides/Enterol2560/E (26).png",
+            type: "Enterol",
+            special_animation: 'Base'
+        },
+        {
+            image: "/slides/Asacol/A (0).png",
+            type: "Asacol",
+            special_animation: 'Base'
+        },
+        {
+            image: "/slides/Stresam2560/C (14).png",
+            type: "Stresam",
+            special_animation: 'Base'
+        },
+        {
+            image: "/slides/Stresam2560/C (1).png",
+            type: "Stresam",
+            special_animation: 'Base'
+        },
+        {
+            image: "/slides/Stresam2560/C (12).png",
+            type: "Stresam",
+            special_animation: 'Base'
+        },
+        {
+            image: "/slides/Stresam2560/C (1).png",
+            type: "Stresam",
+            special_animation: 'Base'
+        }
+    ],
+    pediatric_gastro: [
+        {
+            image: "/slides/Enterol2560/E (1).png",
+            type: "Enterol",
+            special_animation: 'Base'
+        },
+        {
+            image: "/slides/Enterol2560/E (2).png",
+            type: "Enterol",
+            special_animation: 'Base'
+        },
+        {
+            image: "/slides/Enterol2560/E (20).png",
+            type: "Enterol",
+            special_animation: 'Base'
+        },
+        {
+            image: "/slides/Enterol2560/E (19).png",
+            type: "Enterol",
+            special_animation: 'Base'
+        },
+        {
+            image: "/slides/Enterol2560/E (21).png",
+            type: "Enterol",
+            special_animation: 'Base'
+        },
+        {
+            image: "/slides/Enterol2560/E (22).png",
+            type: "Enterol",
+            special_animation: 'Base'
+        },
+        {
+            image: "/slides/Enterol2560/E (26).png",
+            type: "Enterol",
+            special_animation: 'Base'
+        },
+        {
+            image: "/slides/Bebykol2560/Б (4).png",
+            type: "Bebykol",
+            special_animation: 'Base'
+        },
+        {
+            image: "/slides/Bebykol2560/Б (6).png",
+            type: "Bebykol",
+            special_animation: 'Base'
+        },
+        {
+            image: "/slides/Bebykol2560/Б (8).png",
+            type: "Bebykol",
+            special_animation: 'Base'
+        },
+        {
+            image: "/slides/Asacol/A (0).png",
+            type: "Asacol",
+            special_animation: 'Base'
+        }
+    ],
+    advanced_family: [
+        {
+            image: "/slides/Enterol2560/E (1).png",
+            type: "Enterol",
+            special_animation: 'Base'
+        },
+        {
+            image: "/slides/Enterol2560/E (2).png",
+            type: "Enterol",
+            special_animation: 'Base'
+        },
+        {
+            image: "/slides/Enterol2560/E (20).png",
+            type: "Enterol",
+            special_animation: 'Base'
+        },
+        {
+            image: "/slides/Enterol2560/E (19).png",
+            type: "Enterol",
+            special_animation: 'Base'
+        },
+        {
+            image: "/slides/Enterol2560/E (5).png",
+            type: "Enterol",
+            special_animation: 'Base'
+        },
+        {
+            image: "/slides/Enterol2560/E (18).png",
+            type: "Enterol",
+            special_animation: 'Base'
+        },
+        {
+            image: "/slides/Enterol2560/E (21).png",
+            type: "Enterol",
+            special_animation: 'Base'
+        },
+        {
+            image: "/slides/Enterol2560/E (22).png",
+            type: "Enterol",
+            special_animation: 'Base'
+        },
+        {
+            image: "/slides/Enterol2560/E (9).png",
+            type: "Enterol",
+            special_animation: 'Base'
+        },
+        {
+            image: "/slides/Stresam2560/C (2).png",
+            type: "Stresam",
+            special_animation: 'Base'
+        },
+        {
+            image: "/slides/Stresam2560/C (11).png",
+            type: "Stresam",
+            special_animation: 'Base'
+        },
+        {
+            image: "/slides/Stresam2560/C (1).png",
+            type: "Stresam",
+            special_animation: 'Base'
+        },
+        {
+            image: "/slides/Otipax2560/O (1).png",
+            type: "Otipax",
+            special_animation: 'Base'
+        },
+        {
+            image: "/slides/Otipax2560/O (5).png",
+            type: "Otipax",
+            special_animation: 'Base'
+        },
+        {
+            image: "/slides/Otipax2560/O (6).png",
+            type: "Otipax",
+            special_animation: 'Base'
+        },
+        {
+            image: "/slides/Otipax2560/O (7).png",
+            type: "Otipax",
+            special_animation: 'Base'
+        },
+        {
+            image: "/slides/Marimer2560/M (15).png",
+            type: "Marimer",
+            special_animation: 'Base'
+        },
+        {
+            image: "/slides/Marimer2560/M (16).png",
+            type: "Marimer",
+            special_animation: 'Base'
+        },
+        {
+            image: "/slides/ACerumen2560/АЦ (1).png",
+            type: "ACerumen",
+            special_animation: 'Base'
+        }
+    ],
+    beginner_family: [
+        {
+            image: "/slides/Enterol2560/E (1).png",
+            type: "Enterol",
+            special_animation: 'Base'
+        },
+        {
+            image: "/slides/Enterol2560/E (2).png",
+            type: "Enterol",
+            special_animation: 'Base'
+        },
+        {
+            image: "/slides/Enterol2560/E (5).png",
+            type: "Enterol",
+            special_animation: 'Base'
+        },
+        {
+            image: "/slides/Enterol2560/E (6).png",
+            type: "Enterol",
+            special_animation: 'Base'
+        },
+        {
+            image: "/slides/Enterol2560/E (3).png",
+            type: "Enterol",
+            special_animation: 'Base'
+        },
+        {
+            image: "/slides/Enterol2560/E (4).png",
+            type: "Enterol",
+            special_animation: 'Base'
+        },
+        {
+            image: "/slides/Enterol2560/E (7).png",
+            type: "Enterol",
+            special_animation: 'Base'
+        },
+        {
+            image: "/slides/Enterol2560/E (9).png",
+            type: "Enterol",
+            special_animation: 'Base'
+        },
+        {
+            image: "/slides/Stresam2560/C (2).png",
+            type: "Stresam",
+            special_animation: 'Base'
+        },
+        {
+            image: "/slides/Stresam2560/C (11).png",
+            type: "Stresam",
+            special_animation: 'Base'
+        },
+        {
+            image: "/slides/Stresam2560/C (1).png",
+            type: "Stresam",
+            special_animation: 'Base'
+        },
+        {
+            image: "/slides/Otipax2560/O (1).png",
+            type: "Otipax",
+            special_animation: 'Base'
+        },
+        {
+            image: "/slides/Otipax2560/O (5).png",
+            type: "Otipax",
+            special_animation: 'Base'
+        },
+        {
+            image: "/slides/Otipax2560/O (6).png",
+            type: "Otipax",
+            special_animation: 'Base'
+        },
+        {
+            image: "/slides/Otipax2560/O (7).png",
+            type: "Otipax",
+            special_animation: 'Base'
+        },
+        {
+            image: "/slides/Marimer2560/M (15).png",
+            type: "Marimer",
+            special_animation: 'Base'
+        },
+        {
+            image: "/slides/Marimer2560/M (16).png",
+            type: "Marimer",
+            special_animation: 'Base'
+        },
+        {
+            image: "/slides/ACerumen2560/АЦ (2).png",
+            type: "ACerumen",
+            special_animation: 'Base'
+        }
+    ],
+    ent: [
+        {
+            image: "/slides/Marimer2560/M (2).png",
+            type: "Marimer",
+            special_animation: 'Base'
+        },
+        {
+            image: "/slides/Marimer2560/M (18).png",
+            type: "Marimer",
+            special_animation: 'Base'
+        },
+        {
+            image: "/slides/Marimer2560/M (16).png",
+            type: "Marimer",
+            special_animation: 'Base'
+        },
+        {
+            image: "/slides/Marimer2560/M (17).png",
+            type: "Marimer",
+            special_animation: 'Base'
+        },
+        {
+            image: "/slides/Marimer2560/M (23).png",
+            type: "Marimer",
+            special_animation: 'Base'
+        },
+        {
+            image: "/slides/Otipax2560/O (4).png",
+            type: "Otipax",
+            special_animation: 'Base'
+        },
+        {
+            image: "/slides/Otipax2560/O (5).png",
+            type: "Otipax",
+            special_animation: 'Base'
+        },
+        {
+            image: "/slides/Otipax2560/O (6).png",
+            type: "Otipax",
+            special_animation: 'Base'
+        },
+        {
+            image: "/slides/Otipax2560/O (7).png",
+            type: "Otipax",
+            special_animation: 'Base'
+        },
+        {
+            image: "/slides/Otipax2560/O (2).png",
+            type: "Otipax",
+            special_animation: 'Base'
+        },
+        {
+            image: "/slides/ACerumen2560/АЦ (1).png",
+            type: "ACerumen",
+            special_animation: 'Base'
+        },
+        {
+            image: "/slides/ACerumen2560/АЦ (2).png",
+            type: "ACerumen",
+            special_animation: 'Base'
+        },
+        {
+            image: "/slides/Enterol2560/E (9).png",
+            type: "Enterol",
+            special_animation: 'Base'
+        },
+        {
+            image: "/slides/Enterol2560/E (26).png",
+            type: "Enterol",
+            special_animation: 'Base'
+        },
     ]
-};
+}
