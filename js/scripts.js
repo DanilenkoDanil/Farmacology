205ed9445c40912a97a8ff2895f42024a6b6317b
Данил <danilenkodanil74@gmail.com>
2025-01-30 14:33:43 +0200

    scale update

--- js/scripts.js
+++ js/scripts.js
@@ -252,11 +252,13 @@ function applyStyles() {
     if (window.innerWidth > window.innerHeight) {
         if (isTablet2304x1440) {
             document.body.style.transform = "scale(1.41)";
-            document.body.style.inset = "2% auto auto 0";
+            document.body.style.inset = "5% auto auto 0";
         } else if (isTablet2304x1440_minni) {
             document.body.style.transform = "scale(1.41)";
-            document.body.style.inset = "2% auto auto 0";
-
+            document.body.style.inset = "5% auto auto 0";
+        } else if (isTablet2000x1200) {
+            document.body.style.transform = "scale(1.21)";
+            document.body.style.inset = "2% 2% auto 0";
         } else {
             // Сбрасываем стили, если ни одно из условий не выполняется
             document.body.style.transform = "";
